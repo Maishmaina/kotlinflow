4f6b81b2d7cecfe3453904d069c9c0f991ffa54d
Maishmaina <simpledaniel.1818@gmail.com>
2022-11-21 07:54:15 +0300

    simple age calculator

--- app/src/main/java/com/dan/kotlinflow/MainActivity.kt
+++ app/src/main/java/com/dan/kotlinflow/MainActivity.kt
@@ -2,10 +2,24 @@ package com.dan.kotlinflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getAge.setOnClickListener {
+            //make operation.
+            getMyAge()
+
+        }
+    }
+    fun getMyAge(){
+        val userDOB:Int=Integer.parseInt(inputdate.text.toString())
+        val currentYear:Int=Calendar.getInstance().get(Calendar.YEAR)
+        val userAge=currentYear-userDOB
+        seeAge.text="Your Age is $userAge Year"
     }
 }
